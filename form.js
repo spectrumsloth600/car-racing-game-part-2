c6b3a13ea9980fc0084dd50872993eff7c12f103
spectrumsloth600 <61101947+spectrumsloth600@users.noreply.github.com>
2020-06-23 13:10:00 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,43 @@
+class Form {
+    constructor(){
+        this.title = createElement('h2');
+         this.input = createInput("Name");
+        this.button= createButton("Play");
+        this.greeting = createElement('h3');
+
+
+        
+    }
+hide (){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+}
+    display(){
+      
+        this.title.html("SPEED RACER");
+        this.title.position(displayWidth/2-50,0);
+
+        
+         this.input.position(displayWidth/2-50,displayHeight2/80);
+        this.button.position(displayWidth/2+30,displayHeight/2);
+
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+
+            player.name=this.input.value();
+
+            playerCount+=1;
+            player.index=playerCount;
+            player.update(name)
+            player.updateCount(playerCount)
+            
+            this.greeting.html("hello" + name);
+            this.greeting.position(displayWidth/2-50,displayHeight/4);
+            
+
+        });
+    }
+}
